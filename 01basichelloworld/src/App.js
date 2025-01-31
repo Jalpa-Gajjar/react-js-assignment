d74efbb9aa8ddece020d1b7800fbfdca487e8173
Jalpa Vadgama <jalpavadgama52@gmail.com>
2025-01-31 15:02:15 +0530

    hello world

---
+++ 01basichelloworld/src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+    <div className="App-header">
+      <h1>React JS</h1>
+      <img src={logo} className="App-logo" alt="React Logo" />
+    </div>
+    <div className="App-footer">
+      <h2>Hello World</h2>
+    </div>
+  </div>
+  );
+}
+
+export default App;
